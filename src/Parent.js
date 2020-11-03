a51fbf04396ec20a2bb815a351d608fb2088e503
MohammadWaqas908 <66936406+MohammadWaqas908@users.noreply.github.com>
2020-11-03 19:54:26 +0500

    Manage app state with reducer

--- src/Parent.js
+++ src/Parent.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Child from './Child';
-function Parent(props) {
+import Child2 from './Child2';
+function Parent() {
   return (
     <div>
       Parent
-      <Child num={props.num}></Child>
+      <Child></Child>
+      <Child2></Child2>
     </div>
   );
 }
